b65f2febcc33a82227a8bfa79f0a7879e1adaac0
Paty-FN <gttbpatricia2846@gmail.com>
2026-07-01 16:10:39 -0600

    feat: se realizo la actividad mi proyecto

---
+++ mi-proyecto/src/ui.js
@@ -0,0 +1,8 @@
+export const rederizarResultado = (cantidad)=> {
+    const contenedor = document.querySelector("#app");
+        contenedor.innerHTML = `<div class = "tarjeta">
+        <h1><h2>Gestion de usuarios</h2></h1>
+        <p> La cantidad de usuarios es : ${cantidad}</p>
+        </div>
+        `;
+};
